f4a8c54a93b2161e2e31496f8f6296464229e765
krishna <krishna404@yandex.com>
2024-11-09 13:43:38 +0530

    add pwa

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      include: ["**/*.res.mjs"],
+    react({ include: ["**/*.res.mjs"] }),
+    VitePWA({
+      manifest: {
+        name: "Panda notes",
+        short_name: "PandaNotes",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        scope: "/",
+        icons: [{ src: "/vite.svg", type: "image/svg+xml", sizes: "any" }],
+      },
     }),
   ],
 });
